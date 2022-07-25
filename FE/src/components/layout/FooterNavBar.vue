352c79f86a4a84f460b1c7aa4bf80a692e633fc4
kwanukwanu <xoxo70@naver.com>
2022-07-25 14:13:31 +0900

    최재현

--- FE/src/components/layout/FooterNavBar.vue
+++ FE/src/components/layout/FooterNavBar.vue
@@ -1,10 +1,14 @@
 <template>
   <footer class="footer">
-    <h6>
+    <h5>
       <br><br>
-     © 2022 FLODY from SSAFY
+     © 2022 FLODY from SSAFY<br><br></h5>
+     <h6>
+     대전광역시 유성구 동서대로 98-39, 삼성화재 유성연수원 042-820-7385<br>
+     Copyright by Jang Jong Hwan, Kim Ae Ri, Jung Kyo Jun, Jung Se Han, Choi Jae Hyun<br>
+     All rights reserver admin@ssafy.com
     </h6>
-  </footer>
+     </footer>
 </template>
 
 <script>
