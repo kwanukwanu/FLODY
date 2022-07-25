27a7c5936853d8cc576801bfda76339bc47b1689
kwanukwanu <xoxo70@naver.com>
2022-07-25 15:29:17 +0900

    최재현

--- FE/src/components/newspid/NewspidList.vue
+++ FE/src/components/newspid/NewspidList.vue
@@ -26,8 +26,10 @@
             </b-col>
             <b-col>
               <b-row>
-                <div style="width: 90px; display: inline; padding: 4px 0px;">Brown_Cat</div>
-                <div style="width: 200px; display: inline; padding: 4px 0px;">공부를 놓지 않는다. 취한다.</div>
+                <div style="width: 90px; padding: 4px 0px;">Brown_Cat</div>
+              </b-row>
+              <b-row>
+                <div style="width: 200px; padding: 4px 0px;">공부를 놓지 않는다. 취한다.</div>
               </b-row>
             </b-col>
           </b-row>
