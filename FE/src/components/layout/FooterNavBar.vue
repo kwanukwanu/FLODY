fd6f3242448e05c07f67585829dba6a6e21c4006
kwanukwanu <xoxo70@naver.com>
2022-07-25 11:28:42 +0900

    최재현

--- FE/src/components/layout/FooterNavBar.vue
+++ FE/src/components/layout/FooterNavBar.vue
@@ -1,13 +1,26 @@
 <template>
-  <h1>Footer</h1>
+  <footer class="footer">
+    <h6>
+      <br><br>
+     © 2022 FLODY from SSAFY
+    </h6>
+  </footer>
 </template>
 
 <script>
 export default {
 
-}
+};
 </script>
 
 <style>
-
+footer {
+  text-align: center;
+  padding: 1em;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 133px;
+  font-size: smaller;
+}
 </style>
