c61c768c3bdf50444c56143769ef1c2ce2d98c7b
jjong <gfriend0116@naver.com>
2022-07-26 11:21:15 +0900

    build: create spring project

--- BE/src/main/java/com/ssafy/flody/BeApplication.java
+++ BE/src/main/java/com/ssafy/flody/BeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlodyApplication {
+public class BeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FlodyApplication.class, args);
+        SpringApplication.run(BeApplication.class, args);
     }
 
 }
--- BE/src/test/java/com/ssafy/flody/BeApplicationTests.java
+++ BE/src/test/java/com/ssafy/flody/BeApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FlodyApplicationTests {
+class BeApplicationTests {
 
     @Test
     void contextLoads() {
