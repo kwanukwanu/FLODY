a78e3b458d3a84937643bd195911499c3bb6e9a5
jjong <gfriend0116@naver.com>
2022-07-26 11:08:00 +0900

    build: create spring project

---
+++ BE/Flody/src/main/java/com/ssafy/flody/FlodyApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.flody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlodyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlodyApplication.class, args);
+    }
+
+}
