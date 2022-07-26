45e72d06d3c144460ba28e36389fcb266bb418bb
jeongkyojun <kyojun2002@naver.com>
2022-07-26 13:53:28 +0900

    캘린더 수정

--- FE/src/components/study/StudyCalander.vue
+++ FE/src/components/study/StudyCalander.vue
@@ -1,26 +1,34 @@
 <template>
-  <div> Study calandar</div>
-    <calendar></calendar>
-    <DatePicker v-model="date"></DatePicker>
+  <div>Study calandar</div>
+  <DatePicker
+    v-model="dates"
+    :value="null"
+    color="red"
+    style="background-color: #fafafa"
+  ></DatePicker>
   <div>calendar end</div>
 </template>
 
 <script>
-import 'v-calendar/dist/style.css';
-import { Calendar, DatePicker } from 'v-calendar';
+import "v-calendar/dist/style.css";
+import { DatePicker } from "v-calendar";
 export default {
   components: {
-      Calendar,
-      DatePicker,
-    },
-    data() {
-      return {
-        date: new Date(),
-      };
-    },
-}
+    DatePicker,
+  },
+  data() {
+    return {
+      dates: [
+        { start: new Date(2022, 7, 1), end: new Date(2022, 7, 5) },
+        { start: new Date(2022, 7, 10), end: new Date(2022, 7, 15) },
+      ],
+      range: {
+        start: new Date(2022, 0, 1),
+        end: new Date(2022, 0, 5),
+      },
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
