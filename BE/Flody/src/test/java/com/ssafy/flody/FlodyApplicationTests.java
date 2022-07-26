f3951f317bb2903ee18433bd1e3181e45f7b0a73
jjong <gfriend0116@naver.com>
2022-07-26 10:44:52 +0900

    build: create spring project

---
+++ BE/Flody/src/test/java/com/ssafy/flody/FlodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.flody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlodyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
