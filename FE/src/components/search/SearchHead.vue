3d818492eaa130ca760170973aa8d708b9e88ed4
DESKTOP-KVCQHCD\SSAFY <hany961031@naver.com>
2022-07-27 17:06:14 +0900

    update searchview

--- FE/src/components/search/SearchHead.vue
+++ FE/src/components/search/SearchHead.vue
@@ -1,16 +1,16 @@
 <template>
   <h1>자격증 검색</h1>
-  <b-row>
+  <b-row style="margin-bottom: 10px;">
     <b-col>
       <b-form-select style="border-color: #A48282;"></b-form-select>
     </b-col>
     <b-col>
       <b-form-select style="border-color: #A48282;"></b-form-select>
     </b-col>
-    <b-col>
+    <b-col cols="6">
       <b-form-input placeholder="자격증 이름 또는 학과 이름" required style="border-color: #A48282;"></b-form-input>
     </b-col>
-    <b-col>
+    <b-col cols="2">
       <b-button style="color: #453535; background-color: #E1D3D2; border: none">검색</b-button>
     </b-col>
   </b-row>
