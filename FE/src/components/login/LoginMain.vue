3f58cfb7fe38570e09f496e2e013badf17886206
DESKTOP-KVCQHCD\SSAFY <hany961031@naver.com>
2022-07-27 14:40:47 +0900

    update clubSearch

--- FE/src/components/login/LoginMain.vue
+++ FE/src/components/login/LoginMain.vue
@@ -120,7 +120,7 @@
         <h2>스터디 가입하기</h2>
         <b-row>
           <b-col cole="2">
-            <div style="padding: 6px 6px;">스터디 명 : </div>
+            <div style="padding: 6px 6px; font-weight: bold;">스터디 명 : </div>
           </b-col>
           <b-col cols="8">
             <b-form-input required style="border: none; background-color: #F1EBEB;"></b-form-input>
@@ -130,8 +130,83 @@
           </b-col>
         </b-row>
         <hr>
-        <h5>해당 이름을 가진 스터디 목록 (5개씩 페이징 처리)</h5>
-        <b-table striped hover :items="items" :fields="fields"></b-table>
+        <!-- 테이블 -->
+        <h5 style="text-align: left; font-weight: bold;">해당 이름을 가진 스터디 목록 (5개씩 페이징 처리)</h5>
+        <b-table :items="items" :fields="fields" striped responsive="sm">
+          <template #cell(show_details)="row">
+            <b-button size="sm" @click="row.toggleDetails" class="mr-2">
+              {{ row.detailsShowing ? 'Hide' : 'Show'}} Details
+            </b-button>
+
+            <!-- As `row.showDetails` is one-way, we call the toggleDetails function on @change -->
+            <b-form-checkbox v-model="row.detailsShowing" @change="row.toggleDetails">
+              Details via check
+            </b-form-checkbox>
+          </template>
+
+          <template #row-details="row">
+            <b-card>
+              <b-row class="mb-2">
+                <b-col sm="3" class="text-sm-right"><b>Age:</b></b-col>
+                <b-col>{{ row.item.age }}</b-col>
+              </b-row>
+
+              <b-row class="mb-2">
+                <b-col sm="3" class="text-sm-right"><b>Is Active:</b></b-col>
+                <b-col>{{ row.item.isActive }}</b-col>
+              </b-row>
+
+              <b-button size="sm" @click="row.toggleDetails">Hide Details</b-button>
+            </b-card>
+          </template>
+        </b-table>
+        <b-button variant="link" style="color: black; text-decoration: none; font-weight: 100;">&lt; </b-button>
+        <b-button variant="link" style="color: black; text-decoration: none; font-weight: 100;">1</b-button>
+        <b-button variant="link" style="color: black; text-decoration: none; font-weight: 100;">2</b-button>
+        <b-button variant="link" style="color: black; text-decoration: none; font-weight: 100;">3</b-button>
+        <b-button variant="link" style="color: black; text-decoration: none; font-weight: 100;">&gt; </b-button>
+
+        <b-row style="margin-top: 10px;">
+          <b-col style="margin-right: 10px;">
+            <b-row>
+              <h5 style="text-align: left; font-weight: bold;">해당 스터디의 정보 및 소개</h5>
+            </b-row>
+            <b-row style="border: 1px solid; border-color: #A48282; border-radius: 7px;">
+              <div>의지박약 친구들끼리 머리 맞대고 공부하실분들을 찾습니다.</div>
+            </b-row>
+          </b-col>
+          <b-col>
+            <b-row>
+              <h5 style="text-align: left; font-weight: bold;">스터디원 목록</h5>
+            </b-row>
+            <b-row style="border: 1px solid; border-color: #A48282; border-radius: 7px;">
+              <ul style="list-style-position: inside; margin: 0;">
+                <li style="text-align: left; padding-left: 6rem;">Super_Mario</li>
+                <li style="text-align: left; padding-left: 6rem;">Bat_Man</li>
+                <li style="text-align: left; padding-left: 6rem;">Chinderella</li>
+              </ul>
+            </b-row>
+          </b-col>
+        </b-row>
+        <b-row style="margin-top: 10px;">
+          <b-col>
+            <b-row>
+              <h5 style="text-align: left; font-weight: bold;">가입동기</h5>
+            </b-row>
+            <b-row>
+              <b-form-textarea id="textarea" v-model="textEx1" placeholder="Enter something..." rows="3" max-rows="6" style="border: 1px solid; border-color: #A48282;">
+              </b-form-textarea>
+            </b-row>
+          </b-col>
+        </b-row>
+        <b-row align-h="around" style="margin-top: 10px;">
+          <b-col>
+            <b-button text style="color: #453535; background-color: #E1D3D2; border: none">스터디 생성하기</b-button>
+          </b-col>
+          <b-col>
+            <b-button text style="color: #453535; background-color: #E1D3D2; border: none" @click="gotoPage('/study')">신청하기</b-button>
+          </b-col>
+        </b-row>
 
       </b-container>
     </b-card>
@@ -141,20 +216,24 @@
 <script>
 export default{
   data() {
-    return {
-      // Note `isActive` is left out and will not appear in the rendered table
-      fields: ['번호', '스터디 이름', '스터디 장', '체크'],
-      items: [
-        { isActive: true, 번호: 1, '스터디 이름': '정처기 뿌셔', '스터디 장': 'Super_Mario', '체크': true },
-        { isActive: true, 번호: 2, '스터디 이름': '2022 정보처리기사 2회 실기 스터디', '스터디 장': 'Kim', '체크': true },
-        { isActive: true, 번호: 3, '스터디 이름': '정처기 2주 집중 스터디', '스터디 장': 'Speed', '체크': true },
-        { isActive: true, 번호: 4, '스터디 이름': '야 너두 정처기 합격 할 수 있어', '스터디 장': 'Jo', '체크': true },
-      ]
-    }
-  }
+      return {
+        fields: ['first_name', 'last_name', 'show_details'],
+        items: [
+          { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson', _showDetails: true},
+          { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
+        ]
+      }
+  },
+  methods: {
+    gotoPage(link) {
+      console.log(link);
+      this.$router.push(link);
+    },
+  },
 }
 </script>
 
 <style>
-
 </style>
