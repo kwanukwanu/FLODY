0028052b880171b6d2afaf96526c9163ba081431
DESKTOP-KVCQHCD\SSAFY <hany961031@naver.com>
2022-07-28 10:28:46 +0900

    update newspidheader

--- FE/src/components/newspid/NewspidHeader.vue
+++ FE/src/components/newspid/NewspidHeader.vue
@@ -1,5 +1,68 @@
 <template>
-  <h2>Newspid Header</h2>
+  <b-container>
+  <div>내 뉴스피드</div>
+  <b-row align-v="center">
+    <b-col cols="2" style="padding: 0px; text-align: center;">
+      <b-avatar variant="info" src="https://placekitten.com/300/200" size="90px"></b-avatar>
+    </b-col>
+    <b-col >
+      <b-row>
+        <b-col cols="2" style="text-align: left; padding: 0px;">
+          <div style="text-align: left; padding: 0px; font-weight: bold; font-size: 25px;">girl_girls</div>
+        </b-col>
+        <b-col style="text-align: left; padding-left: 4px;">
+          <b-button variant="link" style="color: black; text-decoration: none; font-weight: 100; border: 1px solid;">프로필
+            편집</b-button>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="2" style="text-align: left; padding: 0;">
+          <div>게시글 4</div>
+        </b-col>
+        <b-col cols="2" style="text-align: left; padding: 0;">
+          <div>팔로워 222</div>
+        </b-col>
+        <b-col style="text-align: left; padding: 0;">
+          <div>팔로우 124</div>
+        </b-col>
+      </b-row>
+    </b-col>
+  </b-row>
+  </b-container>
+  <hr style="margin: 5px;" />
+  <b-container>
+  <div>다른사람 뉴스피드</div>
+  <b-row align-v="center">
+    <b-col cols="2" style="padding: 0px; text-align: center;">
+      <b-avatar variant="info" src="https://placekitten.com/300/200" size="90px"></b-avatar>
+    </b-col>
+    <b-col >
+      <b-row>
+        <b-col cols="2" style="text-align: left; padding: 0px;">
+          <div style="text-align: left; padding: 0px; font-weight: bold; font-size: 25px;">girl_girls</div>
+        </b-col>
+        <b-col cols="2" style="text-align: left; padding: 0px;">
+          <b-button variant="link" style="color: black; text-decoration: none; font-weight: 100; border: 1px solid;">메세지 보내기</b-button>
+        </b-col>
+        <b-col style="text-align: left; padding-left: 4px;">
+          <b-button variant="link" style="color: white; background-color: #83A39A; text-decoration: none; font-weight: 100; border: 1px solid;">팔로우</b-button>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="2" style="text-align: left; padding: 0;">
+          <div>게시글 4</div>
+        </b-col>
+        <b-col cols="2" style="text-align: left; padding: 0;">
+          <div>팔로워 222</div>
+        </b-col>
+        <b-col style="text-align: left; padding: 0;">
+          <div>팔로우 124</div>
+        </b-col>
+      </b-row>
+    </b-col>
+  </b-row>
+  </b-container>
+  <hr style="margin: 5px;" />
 </template>
 
 <script>
