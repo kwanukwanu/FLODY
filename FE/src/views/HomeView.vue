1b65935747ecc15a8bae8c2e98d7dcc0f745d3de
jeongkyojun <kyojun2002@naver.com>
2022-08-01 20:59:20 +0900

    front merge

--- FE/src/views/HomeView.vue
+++ FE/src/views/HomeView.vue
@@ -1,16 +1,11 @@
 <template>
   <div class="home">
-<<<<<<< HEAD
-
-=======
->>>>>>> 747b718b466c7827f014d28f1ab752f8ad36098a
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-
 <script>
 export default {
-	name: 'HomeView',
-}
+  name: "HomeView",
+};
 </script>
