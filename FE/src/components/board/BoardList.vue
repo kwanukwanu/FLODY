00430e19335fad0ab21da89b10e36357348782d1
jeongkyojun <kyojun2002@naver.com>
2022-08-02 13:26:29 +0900

    List Detail 수정중

--- FE/src/components/board/BoardList.vue
+++ FE/src/components/board/BoardList.vue
@@ -14,12 +14,26 @@
           </span>
 
           <hr />
-          <board-list-item
-            v-for="(item, index) in items_popular"
-            :key="(item.index = index)"
-            v-bind="item"
-          >
-          </board-list-item>
+          <b-table-simple responsive hover>
+            <b-thead style="background-color: #f8f8ff">
+              <b-tr>
+                <!-- <b-th>카테고리</b-th> -->
+                <b-th>글번호</b-th>
+                <b-th>제목</b-th>
+                <b-th>작성자</b-th>
+                <b-th>작성일</b-th>
+                <b-th>조회수</b-th>
+              </b-tr>
+            </b-thead>
+            <tbody>
+              <board-list-item
+                v-for="(item, index) in items_popular"
+                :key="(item.index = index)"
+                v-bind="item"
+              >
+              </board-list-item>
+            </tbody>
+          </b-table-simple>
           <!-- <b-table fixed hover :items="items_popular" class="common"> </b-table> -->
           <div style="text-align: right">
             <b-button
@@ -125,12 +139,6 @@
     </b-col>
   </b-row>
 
-  <!-- 임시-->
-  <br /><br />
-  <div style="text-align: center">
-    <router-link :to="{ name: 'boarddetail' }">게시물 상세 보기</router-link>
-  </div>
-
   <b-modal
     hide-footer
     id="modal-2"
@@ -193,112 +201,112 @@ export default {
           제목: "오픽 공부 하기 싫다",
           글쓴이: "Super_Mario",
           날짜: "22.07.21",
-          조회수: "7,799",
+          조회수: 7799,
         },
         {
           번호: "101",
           제목: "삼수 끝의 정보처리기사 합격 후기...",
           글쓴이: "Bat_Man",
           날짜: "22.07.21",
-          조회수: "8,677",
+          조회수: 8677,
         },
         {
           번호: "100",
           제목: "toeic speaking honey tip 제공합니다 드루오세요",
           글쓴이: "Cinderellla",
           날짜: "22.07.21",
-          조회수: "4.1만",
+          조회수: 41000,
         },
         {
           번호: "99",
           제목: "정보보안기사 따면 좋나요?",
           글쓴이: "Brown_Cat",
           날짜: "22.07.21",
-          조회수: "2.1만",
+          조회수: 21000,
         },
         {
           번호: "98",
           제목: "오픽 보고 왔는데 네 번 연속 IH.. AL 포기 할까요",
           글쓴이: "Oh_My_Boy",
           날짜: "22.07.21",
-          조회수: "1만",
+          조회수: 10000,
         },
         {
           번호: "97",
           제목: "오픽 공부 하기 싫다ㅜㅜ",
           글쓴이: "Giri_Girl",
           날짜: "22.07.21",
-          조회수: "7,799",
+          조회수: 7799,
         },
         {
           번호: "96",
           제목: "네트워크는 어려워",
           글쓴이: "science_joa",
           날짜: "22.07.21",
-          조회수: "811",
+          조회수: 811,
         },
         {
           번호: "95",
           제목: "토익스피킹 공부하는데 현타가..",
           글쓴이: "Super_Mario",
           날짜: "22.07.21",
-          조회수: "1,334",
+          조회수: 1334,
         },
         {
           번호: "94",
           제목: "정보보안기사 따면 좋나요?",
           글쓴이: "Bat_man",
           날짜: "22.07.21",
-          조회수: "2.1만",
+          조회수: 21000,
         },
         {
           번호: "93",
           제목: "오픽 보고 왔는데 네 번 연속 IH..",
           글쓴이: "heo_gak",
           날짜: "22.07.21",
-          조회수: "1만",
+          조회수: 10000,
         },
         {
           번호: "92",
           제목: "toeic speaking honey tip 제...",
           글쓴이: "eunji",
           날짜: "22.07.21",
-          조회수: "4.1만",
+          조회수: 41000,
         },
         {
           번호: "91",
           제목: "오픽 꿀팁!",
           글쓴이: "jaehyuni",
           날짜: "22.07.21",
-          조회수: "1만",
+          조회수: 10000,
         },
         {
           번호: "90",
           제목: "토익스피킹 나만의 꿀팁..",
           글쓴이: "sehan",
           날짜: "22.07.21",
-          조회수: "3만",
+          조회수: 30000,
         },
         {
           번호: "89",
           제목: "정보보안기사 꿀팁입니다",
           글쓴이: "kyojun",
           날짜: "22.07.21",
-          조회수: "2만",
+          조회수: 20000,
         },
         {
           번호: "88",
           제목: "한능검 꿀팁 드려요",
           글쓴이: "aeri",
           날짜: "22.07.21",
-          조회수: "5,444",
+          조회수: 5444,
         },
         {
           번호: "87",
           제목: "오픽 세 번 만의 AL 달성 후기",
           글쓴이: "jonghwan",
           날짜: "22.07.21",
-          조회수: "1,799",
+          조회수: 1799,
         },
       ],
       page: 1,
--- FE/src/components/board/Items/BoardListItem.vue
+++ FE/src/components/board/Items/BoardListItem.vue
@@ -1,19 +1,35 @@
 <template>
-  <b-row style="padding:5">
-    <b-col style="padding:0" cols="1">번호</b-col>
-    <b-col style="padding:0" cols="5">글 제목</b-col>
-    <b-col style="padding:0" cols="2">작성자</b-col>
-    <b-col style="padding:0" cols="2">작성일</b-col>
-    <b-col style="padding:0" cols="2">조회수</b-col>
-  </b-row>
+  <b-tr style="padding: 5" @click="intoBoard()">
+    <!-- <b-td>{{ 카테고리 }}</b-td> -->
+    <b-td>{{ 번호 }}</b-td>
+    <b-td>{{ 제목 }}</b-td>
+    <b-td>{{ 글쓴이 }}</b-td>
+    <b-td>{{ 날짜 }}</b-td>
+    <b-td>{{ 조회수 }}</b-td>
+  </b-tr>
 </template>
 
 <script>
 export default {
-
-}
+  props: {
+    index: Number,
+    번호: Number,
+    제목: String,
+    글쓴이: String,
+    날짜: String,
+    조회수: Number,
+    내용: String,
+  },
+  methods: {
+    intoBoard() {
+      console.log(this.제목);
+      this.$router.push({
+        path: "/board/Detail",
+        params: { number: this.번호, title: this.제목, author: this.글쓴이 },
+      });
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
