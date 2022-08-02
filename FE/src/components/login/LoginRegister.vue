ab8842f8cf818c3d70e92007863016d30bcc8625
DESKTOP-KVCQHCD\SSAFY <hany961031@naver.com>
2022-08-02 11:18:19 +0900

    add social login firebase

--- FE/src/components/login/LoginRegister.vue
+++ FE/src/components/login/LoginRegister.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
-    <b-button>Social Login</b-button>
-    <p><input type="text" placeholder="Email" v-model="email" /></p>
-    <p><input type="password" placeholder="Password" v-model="password" /></p>
-    <p><button @click="register">Submit</button></p>
+    <h2 v-if="user">Signed In User : {{ user }}</h2>
+    <div id="firebaseui-auth-container"></div>
+    <div id="loader">Loading...</div>
+    <br />
+    <div v-if="isSignedIn">
+      <button @click="handleSignedOut">로그아웃</button>
+    </div>
   </div>
 </template>
 
 <script>
-// import { ref } from 'vue'
-// import firebase from 'firebase'
+import { ref } from 'vue';
+import firebaseConfig from '@/firebaseConfig';
+import firebase from 'firebase/compat/app';
+
+firebase.initializeApp(firebaseConfig);
+
+import * as firebaseui from 'firebaseui';
+import 'firebaseui/dist/firebaseui.css';
+
+import { getAuth, signOut } from "firebase/auth";
+const auth = getAuth();
+
+
+// import { useRoute } from 'vue-router';
+
 // import { useRouter } from 'vue-router' // import router
 // const email = ref('')
 // const password = ref('')
@@ -28,9 +44,63 @@
 //     });
 // }
 
-// export default {
-  
-// }
+export default {
+  setup() {
+    const user = ref(null);
+    const isSignedIn = ref(false);
+
+    const uiConfig = {
+      signInFlow: 'popup',
+      signInSuccessUrl: "http://localhost:8080/main",
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        //firebase.auth.GithubAuthProvider.PROVIDER_ID
+      ],
+      callbacks: {
+        signInSuccessWithAuthResult: function(authResult) {
+          user.value = authResult.user.displayName;
+          console.log(authResult);
+          isSignedIn.value = true;
+          console.log('Signed in by user ' + user.value);
+
+          // so it doesn't refresh the page
+          return false;
+        },
+        uiShown: function() {
+          // The widget is rendered.
+          // Hide the loader.
+          document.getElementById('loader').style.display = 'none';
+        }
+      }
+    }
+
+    // Initialize the FirebaseUI Widget using Firebase.
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+    ui.start('#firebaseui-auth-container', uiConfig);
+
+    const handleSignedOut = () => {
+      signOut(auth).then(() => {
+        // Sign-out successful.
+        user.value = null;
+        isSignedIn.value = false;
+        console.log('signed out');
+        ui.start('#firebaseui-auth-container', uiConfig);
+      }).catch((error) => {
+        // An error happened.
+        console.log(error);
+      });
+    }
+
+    return {
+      user,
+      isSignedIn,
+      handleSignedOut
+    }
+  }
+}
 </script>
 
 <style>
