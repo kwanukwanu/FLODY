295012528bacc6097ca7c155c7008b58a181744f
DESKTOP-KVCQHCD\SSAFY <hany961031@naver.com>
2022-08-03 17:22:13 +0900

    update flowerField

---
+++ FE/src/components/study/StudyField.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>field</h2>
+  <div style="background-image: url(../../assets/field.png);">
+    <img src="@/assets/julgi.png" class="w-25" style="height: 240px; width: 200px; border: solid 3px; border-radius: 100%; border-color: green;"/>
+  </div>
+  <img src="@/assets/field.png"/>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+
+<style>
+
+</style>
--- FE/src/views/StudyView.vue
+++ FE/src/views/StudyView.vue
@@ -12,14 +12,18 @@
       <study-todo></study-todo>
     </b-col>
   </b-row>
+  <b-row>
+    <study-field></study-field>
+  </b-row>
 </template>
 
 <script>
 import StudyCalender from '@/components/study/StudyCalender.vue'
 import StudyTodo from '@/components/study/StudyTodo.vue'
 import StudyFlower from '@/components/study/StudyFlower.vue'
+import StudyField from '@/components/study/StudyField.vue'
 export default {
-  components: { StudyCalender,StudyTodo, StudyFlower },
+  components: { StudyCalender, StudyTodo, StudyFlower, StudyField },
 
 }
 </script>
