c8f3f383b3281c498eaa916c10d818153bb0cb86
DESKTOP-KVCQHCD\SSAFY <hany961031@naver.com>
2022-08-04 14:37:19 +0900

    update flower

--- FE/src/components/study/StudyField.vue
+++ FE/src/components/study/StudyField.vue
@@ -1,37 +1,66 @@
 <template>
   <h2>field</h2>
-  <div id="fi">
-    <img src="@/assets/flower7_1.png" class="w-10" style="height: 100px; cursor: grab;" @mousedown="startDrag()"/>
+  <div id="fi" @click="action_coords($event)">
+    <img id="flower" src="@/assets/flower7_1.png" class="w-10" style="height: 100px; cursor: grab;" @mousedown="startDrag($event)"/>
   </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            pos:{
-                posX:0,
-                posY:0,
-            },
-        }
-    },
-    methods: {
+  data(){
+    return{
+        pos:{
+            posX:0,
+            posY:0,
+      },
+    }
+  },
+  methods: {
     startDrag(){
-        /*
-        드래그를 할때
-        posX 에는 그림에대한 x 위치를,
-        posy 에는 그림에 대한 y 위치를 받아온다
-        */
+      /*
+      드래그를 할때
+      posX 에는 그림에대한 x 위치를,
+      posy 에는 그림에 대한 y 위치를 받아온다
+      */
+      
+      const bg = document.getElementById('fi');
+      const flower = document.getElementById('flower');
+      let coordBg = bg.getBoundingClientRect();
+      let coordFl = flower.getBoundingClientRect();
 
+      console.log(coordFl);
+      console.log((coordFl.top + (coordFl.height / 2)) - coordBg.top);
+      console.log((coordFl.left + (coordFl.width / 2)) - coordBg.left);
 
-        // posX = getLeft(obj) - e_obj.clientX;
-        // posY = getTop(obj) - e_obj.clientY;
+      let x1 = event.clientX; 
+      let y1 = event.clientY; 
+      let x2 = event.offsetX; 
+      let y2 = event.offsetY; 
+      let x3 = event.screenX; 
+      let y3 = event.screenY; 
+      console.log("flower : clientX: " + x1 + ", clientY: " + y1);
+      console.log("flower : offsetX: " + x2 + ", offsetY: " + y2);
+      console.log("flower : screenX: " + x3 + ", screenY: " + y3); 
 
-        // document.onmousemove = moveDrag;
-        // document.onmouseup = stopDrag;
-        // if(e_obj.preventDefault)e_obj.preventDefault();
-        }
+      // posX = getLeft(obj) - e_obj.clientX;
+      // posY = getTop(obj) - e_obj.clientY;
+
+      // document.onmousemove = moveDrag;
+      // document.onmouseup = stopDrag;
+      // if(e_obj.preventDefault)e_obj.preventDefault();
     },
+    action_coords(event) { 
+      let x1 = event.clientX; 
+      let y1 = event.clientY; 
+      let x2 = event.offsetX; 
+      let y2 = event.offsetY; 
+      let x3 = event.screenX; 
+      let y3 = event.screenY; 
+      console.log("clientX: " + x1 + ", clientY: " + y1);
+      console.log("offsetX: " + x2 + ", offsetY: " + y2);
+      console.log("screenX: " + x3 + ", screenY: " + y3); 
+    },
+  },
 }
 
 </script>
