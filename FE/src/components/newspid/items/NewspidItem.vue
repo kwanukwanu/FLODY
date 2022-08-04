d2ff17bb5a53fd1eea33ae78d0886810b0aec839
jeongkyojun <kyojun2002@naver.com>
2022-08-04 09:41:53 +0900

    flower pictures

--- FE/src/components/newspid/items/NewspidItem.vue
+++ FE/src/components/newspid/items/NewspidItem.vue
@@ -1,13 +1,43 @@
 <template>
-  <div></div>
+  <b-row>
+    <b-col cols="2" style="padding: 4px">
+      <div class="avatar">
+        <b-avatar
+          variant="info"
+          src="https://placekitten.com/300/300"
+          size="50px"
+        ></b-avatar>
+      </div>
+    </b-col>
+    <b-col>
+      <b-row>
+        <div style="text-align: left; padding: 0px; font-weight: bold">
+          Brown_Cat
+        </div>
+      </b-row>
+      <b-row>
+        <div style="text-align: left; padding: 0px">
+          공부를 놓지 않는다. 취한다.
+        </div>
+      </b-row>
+      <b-row>
+        <div style="text-align: left; padding: 0px; color: #255d4e">
+          #영국 #런던 #스터디
+        </div>
+      </b-row>
+      <b-row style="margin-bottom: 5px">
+        <div
+          style="text-align: left; padding: 0px; color: gray; font-size: small"
+        >
+          15시간전
+        </div>
+      </b-row>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
