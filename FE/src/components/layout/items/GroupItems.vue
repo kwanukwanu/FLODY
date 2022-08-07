d5f3d4d19cf75e9795a52ac56c160273b84935ce
jeongkyojun <kyojun2002@naver.com>
2022-08-07 18:13:40 +0900

    Navbar 수정

---
+++ FE/src/components/layout/items/GroupItems.vue
@@ -0,0 +1,22 @@
+<template>
+    <li style="text-align: left;">
+        <b-avatar size="30px" :src="src" style="margin: 0px 5px 5px 0px;">
+        </b-avatar>
+        {{ id }}
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        id: String,
+        src: String,
+    },
+    mounted() {
+        console.log(this.id + " , " + this.src);
+    }
+}
+</script>
+
+<style>
+</style>
