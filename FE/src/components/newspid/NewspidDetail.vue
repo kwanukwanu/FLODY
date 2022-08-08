72d9bfc512db717fbd17af41800b518c450c61ef
DESKTOP-KVCQHCD\SSAFY <hany961031@naver.com>
2022-08-08 09:27:28 +0900

    update modal cancel

--- FE/src/components/newspid/NewspidDetail.vue
+++ FE/src/components/newspid/NewspidDetail.vue
@@ -32,7 +32,7 @@
   <b-modal hide-footer hide-title hide-header  centered id="modal-4" size="sm" style="text-align: center;">
     <b-container>
       <!-- 본인 게시물이라면 신고 안 보이도록 해야 함 -->
-      <b-row @click="Delete" style="text-align:center; cursor: pointer; color: red; font-weight: 600;">
+      <b-row @click="Delete" style="text-align:center; cursor: pointer; color: red; font-weight: 600;" data-bs-dismiss="modal" aria-label="Close">
         <div style="text-align:center;" v-b-modal.modal-7>신고</div>
       </b-row>
       <hr />
@@ -44,7 +44,7 @@
         <div style="text-align:center;">수정</div>
       </b-row>
       <hr />
-      <b-row @click="Cancel" style="text-align:center; cursor: pointer;">
+      <b-row @click="Cancel" style="text-align:center; cursor: pointer;" data-bs-dismiss="modal" aria-label="Close">
         <div style="text-align:center;">취소</div>
       </b-row>
     </b-container>
@@ -70,7 +70,7 @@
         <div style="text-align:center;">팔로우 취소</div>
       </b-row>
       <hr />
-      <b-row @click="Cancel" style="text-align:center; cursor: pointer;">
+      <b-row @click="Cancel" style="text-align:center; cursor: pointer;" data-bs-dismiss="modal" aria-label="Close">
         <div style="text-align:center;" >취소</div>
       </b-row>
     </b-container>
