7ade654e7e94d7eec024b3131af43fb477c203a9
kwanukwanu <xoxo70@naver.com>
2022-08-09 14:35:31 +0900

    study todo 모달 우선 복사

--- FE/src/components/study/StudyTodo.vue
+++ FE/src/components/study/StudyTodo.vue
@@ -63,6 +63,116 @@
 
           </ul>
         </b-card>
+
+         <b-modal hide-footer id="modal-8" centered no-stacking title="할 일 등록" style="text-align: center">
+    <b-card style="min-height: 12rem; max-width: 30rem; background-color: #f8f3f3">
+      <b-container ref="form">
+        <!-- <h2 style="text-align: center;">목표 등록</h2> -->
+        <b-row style="margin-bottom: 20px">
+          <b-col cols="11">
+            <b-form-input placeholder="내용 입력..." required style="border: none"></b-form-input>
+          </b-col>
+          <b-col cols="1">
+            <span style="vertical-align: middle;">
+              <b-button size="sm" style="border:none; background:none; padding: 0px 0px">
+                <svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M10 6.667v6.666M6.667 10h6.666m5 0a8.333 8.333 0 1 1-16.666 0 8.333 8.333 0 0 1 16.666 0Z"
+                    stroke="#444" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+              </b-button>
+            </span>
+          </b-col>
+        </b-row>
+
+        <b-row style="margin-bottom: 20px">
+          <b-col cols="11">
+            <b-form-input placeholder="내용 입력..." required style="border: none"></b-form-input>
+          </b-col>
+          <b-col cols="1">
+            <span style="vertical-align: middle;">
+              <b-button size="sm" style="border:none; background:none; padding: 0px 0px">
+                <svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M10 6.667v6.666M6.667 10h6.666m5 0a8.333 8.333 0 1 1-16.666 0 8.333 8.333 0 0 1 16.666 0Z"
+                    stroke="#444" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+              </b-button>
+            </span>
+          </b-col>
+        </b-row>
+
+        <b-row style="margin-bottom: 20px">
+          <b-col cols="11">
+            <b-form-input placeholder="내용 입력..." required style="border: none"></b-form-input>
+          </b-col>
+          <b-col cols="1">
+            <span style="vertical-align: middle;">
+              <b-button size="sm" style="border:none; background:none; padding: 0px 0px">
+                <svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M10 6.667v6.666M6.667 10h6.666m5 0a8.333 8.333 0 1 1-16.666 0 8.333 8.333 0 0 1 16.666 0Z"
+                    stroke="#444" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+              </b-button>
+            </span>
+          </b-col>
+        </b-row>
+
+        <b-row style="margin-bottom: 20px">
+          <b-col cols="11">
+            <b-form-input placeholder="내용 입력..." required style="border: none"></b-form-input>
+          </b-col>
+          <b-col cols="1">
+            <span style="vertical-align: middle;">
+              <b-button size="sm" style="border:none; background:none; padding: 0px 0px">
+                <svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M10 6.667v6.666M6.667 10h6.666m5 0a8.333 8.333 0 1 1-16.666 0 8.333 8.333 0 0 1 16.666 0Z"
+                    stroke="#444" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+              </b-button>
+            </span>
+          </b-col>
+        </b-row>
+
+        <b-row style="margin-bottom: 20px">
+          <b-col cols="11">
+            <b-form-input placeholder="내용 입력..." required style="border: none"></b-form-input>
+          </b-col>
+          <b-col cols="1">
+            <span style="vertical-align: middle;">
+              <b-button size="sm" style="border:none; background:none; padding: 0px 0px">
+                <svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M10 6.667v6.666M6.667 10h6.666m5 0a8.333 8.333 0 1 1-16.666 0 8.333 8.333 0 0 1 16.666 0Z"
+                    stroke="#444" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+              </b-button>
+            </span>
+          </b-col>
+        </b-row>
+        <!-- <b-row style="margin-bottom: 20px">
+          <b-col cols="2">
+            <span style="vertical-align:middle; font-weight: bold;">시작</span>
+          </b-col>
+          <b-col cols="4">
+            <input type="time" value="xxx" min="yyy" max="zzz">
+          </b-col>
+          <b-col cols="2">
+            <span style="vertical-align:middle; font-weight: bold;">종료</span>
+          </b-col>
+          <b-col cols="4">
+            <input type="time" value="xxx" min="yyy" max="zzz">
+          </b-col>
+        </b-row> -->
+        <!-- <b-row>
+          <b-col>
+            <b-form-textarea id="content" placeholder="내용 입력..." rows="7" max-rows="15" required style="border: none">
+            </b-form-textarea>
+          </b-col>
+        </b-row> -->
+        <br />
+        <b-button text @click="submit" style="color: #453535; background-color: #e1d3d2; border: none">등록</b-button>
+      </b-container>
+      <br />
+    </b-card>
+  </b-modal>
 </template>
 
 <script>
