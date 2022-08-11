cced24345be5f4b66f667201f99ce2355a7de4b4
jeongkyojun <kyojun2002@naver.com>
2022-08-11 15:38:52 +0900

    중간점검

---
+++ FE/src/components/group/items/GroupTodoItems.vue
@@ -0,0 +1,19 @@
+<template>
+    <li>
+        <div class="checkbox">
+            <input type="checkbox" name="packersOff" id="packers" value="1" />
+            <label for="packers" class="strikethrough">&nbsp;네트워크 뽀사기</label>
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+
+    }
+}
+</script>
+
+<style>
+</style>
