c5e49e78f072346aad9b127f74767d5e1500426e
kwanukwanu <xoxo70@naver.com>
2022-08-11 17:24:39 +0900

    팔로우 팔로잉 모달

--- FE/src/components/study/StudyTodo.vue
+++ FE/src/components/study/StudyTodo.vue
@@ -1,152 +1,110 @@
 <template>
   <b-card style="border-color: #a48282; max-width: 35rem; height:304px; overflow-y: scroll;">
-          <b-row>
-            <b-col cols="10">
-              <span
-                style="text-align:left; font-weight:bold; font-size: large; vertical-align:-webkit-baseline-middle;">Todo
-                List</span>
-            </b-col>
-            <b-col cols="2">
-              <span style="text-align:right;">
-                <b-button size="sm" style="background-color:white; border-color:white;" v-b-modal.modal-8><svg
-                    width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12 5v14m-7-7h14" stroke="#444" stroke-width="2" stroke-linecap="round"
-                      stroke-linejoin="round" />
-                  </svg></b-button>
-              </span>
-            </b-col>
-          </b-row>
-          <hr />
-          <ul style="list-style-type :none;">
-            <li>
-              <div class="checkbox">
-                <input type="checkbox" name="packersOff" id="packers" value="1" />
-                <label for="packers" class="strikethrough">&nbsp;네트워크 뽀사기</label>
-              </div>
-            </li>
-            <li>
-              <div class="checkbox">
-                <input type="checkbox" name="packersOff" id="packers_2" value="2" />
-                <label for="packers_2" class="strikethrough">&nbsp;보안 유형 정복</label>
-              </div>
-            </li>
-            <li>
-              <div class="checkbox">
-                <input type="checkbox" name="packersOff" id="packers_3" value="3" />
-                <label for="packers_3" class="strikethrough">&nbsp;21년 2회차 실기 풀이</label>
-              </div>
-            </li>
-            <li>
-              <div class="checkbox">
-                <input type="checkbox" name="packersOff" id="packers_4" value="4" />
-                <label for="packers_4" class="strikethrough">&nbsp;java 뽀사기</label>
-              </div>
-            </li>
-            <li>
-              <div class="checkbox">
-                <input type="checkbox" name="packersOff" id="packers_5" value="5" />
-                <label for="packers_5" class="strikethrough">&nbsp;python 유형 정복</label>
-              </div>
-            </li>
-            <li>
-              <div class="checkbox">
-                <input type="checkbox" name="packersOff" id="packers_6" value="6" />
-                <label for="packers_6" class="strikethrough">&nbsp;22년 2회차 실기 풀이</label>
-              </div>
-            </li>
-            <li>
-              <div class="checkbox">
-                <input type="checkbox" name="packersOff" id="packers_7" value="7" />
-                <label for="packers_7" class="strikethrough">&nbsp;3차 스터디 회의</label>
-              </div>
-            </li>
+    <b-row>
+      <b-col cols="10">
+        <span style="text-align:left; font-weight:bold; font-size: large; vertical-align:-webkit-baseline-middle;">Todo
+          List</span>
+      </b-col>
+      <b-col cols="2">
+        <span style="text-align:right;">
+          <b-button size="sm" style="background-color:white; border-color:white;" v-b-modal.modal-8><svg width="24"
+              height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12 5v14m-7-7h14" stroke="#444" stroke-width="2" stroke-linecap="round"
+                stroke-linejoin="round" />
+            </svg></b-button>
+        </span>
+      </b-col>
+    </b-row>
+    <hr />
+    <ul style="list-style-type :none;">
+      <li>
+        <div class="checkbox">
+          <input type="checkbox" name="packersOff" id="packers" value="1" />
+          <label for="packers" class="strikethrough">&nbsp;네트워크 뽀사기</label>
+        </div>
+      </li>
+      <li>
+        <div class="checkbox">
+          <input type="checkbox" name="packersOff" id="packers_2" value="2" />
+          <label for="packers_2" class="strikethrough">&nbsp;보안 유형 정복</label>
+        </div>
+      </li>
+      <li>
+        <div class="checkbox">
+          <input type="checkbox" name="packersOff" id="packers_3" value="3" />
+          <label for="packers_3" class="strikethrough">&nbsp;21년 2회차 실기 풀이</label>
+        </div>
+      </li>
+      <li>
+        <div class="checkbox">
+          <input type="checkbox" name="packersOff" id="packers_4" value="4" />
+          <label for="packers_4" class="strikethrough">&nbsp;java 뽀사기</label>
+        </div>
+      </li>
+      <li>
+        <div class="checkbox">
+          <input type="checkbox" name="packersOff" id="packers_5" value="5" />
+          <label for="packers_5" class="strikethrough">&nbsp;python 유형 정복</label>
+        </div>
+      </li>
+      <li>
+        <div class="checkbox">
+          <input type="checkbox" name="packersOff" id="packers_6" value="6" />
+          <label for="packers_6" class="strikethrough">&nbsp;22년 2회차 실기 풀이</label>
+        </div>
+      </li>
+      <li>
+        <div class="checkbox">
+          <input type="checkbox" name="packersOff" id="packers_7" value="7" />
+          <label for="packers_7" class="strikethrough">&nbsp;3차 스터디 회의</label>
+        </div>
+      </li>
 
-          </ul>
-        </b-card>
+    </ul>
+  </b-card>
 
-         <b-modal hide-footer id="modal-8" centered no-stacking title="할 일 등록" style="text-align: center">
+  <b-modal hide-footer id="modal-8" centered no-stacking title="할 일 등록" style="text-align: center">
     <b-card style="min-height: 12rem; max-width: 30rem; background-color: #f8f3f3">
       <b-container ref="form">
         <!-- <h2 style="text-align: center;">목표 등록</h2> -->
-        <b-row style="margin-bottom: 20px">
-          <b-col cols="11">
-            <b-form-input placeholder="내용 입력..." required style="border: none"></b-form-input>
-          </b-col>
-          <b-col cols="1">
-            <span style="vertical-align: middle;">
-              <b-button size="sm" style="border:none; background:none; padding: 0px 0px">
-                <svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M10 6.667v6.666M6.667 10h6.666m5 0a8.333 8.333 0 1 1-16.666 0 8.333 8.333 0 0 1 16.666 0Z"
-                    stroke="#444" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-              </b-button>
-            </span>
-          </b-col>
-        </b-row>
+        <b-table-simple>
+          <tbody id="mytest">
+            <tr style="margin-bottom: 20px">
+              <td cols="11">
+                <b-form-input id="holder" placeholder="내용 입력..." required style="border: none"></b-form-input>
+              </td>
+              <td cols="1">
+                <span id="add" style="vertical-align: middle;">
+                  <b-button size="sm" style="border:none; background:none; padding: 0px 0px" @click="addRow()">
+                    <svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg" style="color:red;">
+                      <path
+                        d="M10 6.667v6.666M6.667 10h6.666m5 0a8.333 8.333 0 1 1-16.666 0 8.333 8.333 0 0 1 16.666 0Z"
+                        stroke="#444" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                  </b-button>
+                </span>
+              </td>
+            </tr>
+          </tbody>
+          <tbody id="mytest_2">
+            <tr style="margin-bottom: 20px">
+              <td cols="11">
+                <b-form-input id="holder" placeholder="내용 입력..." required style="border: none"></b-form-input>
+              </td>
+              <td cols="1">
+                <span id="delete" style="vertical-align: middle;">
+                  <b-button size="sm" style="border:none; background:none; padding: 0px 0px" @click="deleteRow()">
+                    <svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M6.667 10h6.666m5 0a8.333 8.333 0 1 1-16.666 0 8.333 8.333 0 0 1 16.666 0Z" stroke="#444"
+                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                  </b-button>
+                </span>
+              </td>
+            </tr>
+          </tbody>
+        </b-table-simple>
 
-        <b-row style="margin-bottom: 20px">
-          <b-col cols="11">
-            <b-form-input placeholder="내용 입력..." required style="border: none"></b-form-input>
-          </b-col>
-          <b-col cols="1">
-            <span style="vertical-align: middle;">
-              <b-button size="sm" style="border:none; background:none; padding: 0px 0px">
-                <svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M10 6.667v6.666M6.667 10h6.666m5 0a8.333 8.333 0 1 1-16.666 0 8.333 8.333 0 0 1 16.666 0Z"
-                    stroke="#444" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-              </b-button>
-            </span>
-          </b-col>
-        </b-row>
-
-        <b-row style="margin-bottom: 20px">
-          <b-col cols="11">
-            <b-form-input placeholder="내용 입력..." required style="border: none"></b-form-input>
-          </b-col>
-          <b-col cols="1">
-            <span style="vertical-align: middle;">
-              <b-button size="sm" style="border:none; background:none; padding: 0px 0px">
-                <svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M10 6.667v6.666M6.667 10h6.666m5 0a8.333 8.333 0 1 1-16.666 0 8.333 8.333 0 0 1 16.666 0Z"
-                    stroke="#444" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-              </b-button>
-            </span>
-          </b-col>
-        </b-row>
-
-        <b-row style="margin-bottom: 20px">
-          <b-col cols="11">
-            <b-form-input placeholder="내용 입력..." required style="border: none"></b-form-input>
-          </b-col>
-          <b-col cols="1">
-            <span style="vertical-align: middle;">
-              <b-button size="sm" style="border:none; background:none; padding: 0px 0px">
-                <svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M10 6.667v6.666M6.667 10h6.666m5 0a8.333 8.333 0 1 1-16.666 0 8.333 8.333 0 0 1 16.666 0Z"
-                    stroke="#444" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-              </b-button>
-            </span>
-          </b-col>
-        </b-row>
-
-        <b-row style="margin-bottom: 20px">
-          <b-col cols="11">
-            <b-form-input placeholder="내용 입력..." required style="border: none"></b-form-input>
-          </b-col>
-          <b-col cols="1">
-            <span style="vertical-align: middle;">
-              <b-button size="sm" style="border:none; background:none; padding: 0px 0px">
-                <svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M10 6.667v6.666M6.667 10h6.666m5 0a8.333 8.333 0 1 1-16.666 0 8.333 8.333 0 0 1 16.666 0Z"
-                    stroke="#444" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-              </b-button>
-            </span>
-          </b-col>
-        </b-row>
         <!-- <b-row style="margin-bottom: 20px">
           <b-col cols="2">
             <span style="vertical-align:middle; font-weight: bold;">시작</span>
@@ -173,12 +131,30 @@
       <br />
     </b-card>
   </b-modal>
+
+
+
 </template>
 
 <script>
 export default {
+  methods:{
+    addRow() {
+    var tbody = document.getElementById('mytest_2');
+    var row = tbody.insertRow(tbody.rows.length); // 하단에 추가
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = document.getElementById('holder').outerHTML;
+    cell2.innerHTML = document.getElementById('delete').outerHTML;
+    },
 
+    deleteRow() {
+    var tbody = document.getElementById('mytest_2');
+    tbody.deleteRow(this.row);
+    },
+    }
 }
+
 </script>
 
 <style>
