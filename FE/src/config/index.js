09f6a44cc884420ecf70678c5e65aee3d5732b0f
DESKTOP-KVCQHCD\SSAFY <hany961031@naver.com>
2022-08-17 13:36:07 +0900

    update goalDelete

--- FE/src/config/index.js
+++ FE/src/config/index.js
@@ -1,5 +1,5 @@
 // 관련 데이터 URL을 저장하는 부분
-// const API_BASE_URL = "https://i7b101.p.ssafy.io/api";
-const API_BASE_URL = "http://localhost:8081/api";
+const API_BASE_URL = "https://i7b101.p.ssafy.io/api";
+// const API_BASE_URL = "http://localhost:8081/api";
 
 export { API_BASE_URL };
