d403f0e392d757a7487263678f96720e24862ddd
jeongkyojun <kyojun2002@naver.com>
2022-08-19 10:47:25 +0900

    userGoal 삭제

--- FE/src/components/layout/items/MyPageItems.vue
+++ FE/src/components/layout/items/MyPageItems.vue
@@ -10,10 +10,18 @@
 </template>
 
 <script>
+import { useStore } from "vuex";
+import { delete_goal } from "@/api/goal";
+
 export default {
     props: {
         title: String,
         dueDate: String,
+        ugNo: Number,
+    },
+    setup() {
+        const store = useStore();
+        return { store };
     },
     mounted() {
         console.log(this.title + " , " + this.dueDate);
@@ -30,30 +38,46 @@ export default {
         }
     },
     methods: {
-      deleteGoal() {
-        // let result = confirm("Do you want to delete this goal?");
-        // if(result == true) {
-        //   alert("삭제");
-        // }
-        this.$swal.fire({
-                    title: '정말로 그렇게 하시겠습니까?',
-                    text: "다시 되돌릴 수 없습니다. 신중하세요.",
-                    icon: 'error',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: '삭제',
-                    cancelButtonText: '취소'
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        this.$swal.fire(
-                            '승인이 완료되었습니다.',
-                            '화끈하시네요~!',
-                            'success'
-                        )
-                    }
-                })
-      }
+        deleteGoal() {
+            // let result = confirm("Do you want to delete this goal?");
+            // if(result == true) {
+            //   alert("삭제");
+            // }
+            this.$swal.fire({
+                title: '정말로 그렇게 하시겠습니까?',
+                text: "다시 되돌릴 수 없습니다. 신중하세요.",
+                icon: 'error',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: '삭제',
+                cancelButtonText: '취소'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    console.log(this.ugNo);
+                    delete_goal(
+                        this.ugNo,
+                        (response) => {
+                            if (response.data.msg === "SUCCESS") {
+                                this.$swal.fire(
+                                    '승인이 완료되었습니다.',
+                                    '화끈하시네요~!',
+                                    'success'
+                                );
+                                this.getPlans();
+                            }
+                        },
+                        (error) => {
+                            console.log(error);
+                        }
+                    )
+                }
+            })
+        },
+        async getPlans() {
+            // 여기서 axios를 통해 목표를 받아온다.
+            await this.store.dispatch("memberStore/setgoals");
+        },
     }
 }
 </script>
